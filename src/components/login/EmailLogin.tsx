38e636c3600b458be9958992e9f809781f1464d8
Ross Yeager <ryeager12@gmail.com>
2023-05-21 22:05:54 -0700

    add in email login mechanism

---
+++ src/components/login/EmailLogin.tsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
+
+interface LoginModalProps {
+  open: boolean;
+  onClose: () => void;
+  onLogin: (email: string, password: string) => void;
+  onRegister: (email: string, password: string) => void;
+}
+
+const EmailLoginModal: React.FC<LoginModalProps> = ({
+  open,
+  onClose,
+  onLogin,
+  onRegister,
+}) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = () => {
+    onLogin(email, password);
+    onClose();
+  };
+
+  const handleRegister = () => {
+    onRegister(email, password);
+    onClose();
+  };
+
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle>Login or Create Account</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Please enter your email and password to login or create a new account.
+        </DialogContentText>
+        <TextField
+          autoFocus
+          margin="dense"
+          label="Email"
+          type="email"
+          fullWidth
+          value={email}
+          onChange={handleEmailChange}
+        />
+        <TextField
+          margin="dense"
+          label="Password"
+          type="password"
+          fullWidth
+          value={password}
+          onChange={handlePasswordChange}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleLogin}>Login</Button>
+        <Button onClick={handleRegister}>Register</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default EmailLoginModal;
--- src/hooks/firebase/auth.ts
+++ src/hooks/firebase/auth.ts
@@ -1,6 +1,26 @@
 import { useEffect, useState } from "react";
-import { getAuth, User } from "firebase/auth";
+import {
+  getAuth,
+  User,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from "firebase/auth";
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  doc,
+  where,
+  setDoc,
+} from "firebase/firestore";
 import myApp from "firebaseApp";
+
+import { ClientConfig } from "types/user";
 
 export const useAuthStateWatcher = () => {
   const auth = getAuth(myApp);
@@ -12,4 +32,74 @@ export const useAuthStateWatcher = () => {
   }, [auth]);
 
   return user;
+};
+
+interface EmailLoginProps {
+  email: string;
+  password: string;
+}
+
+interface RegisterEmailProps extends EmailLoginProps {
+  name: string;
+  collection_name: string;
+  default_config: ClientConfig;
+}
+
+export const signInWithGoogle = async (collection_name: string, default_config: ClientConfig) => {
+  try {
+    const db = getFirestore(myApp);
+    const res = await signInWithPopup(getAuth(myApp), new GoogleAuthProvider());
+    const user = res.user;
+    const q = query(collection(db, collection_name), where("uid", "==", user.uid));
+    const docs = await getDocs(q);
+    if (docs.docs.length === 0 && user.email) {
+      await setDoc(doc(collection(db, collection_name), user.email), default_config);
+    }
+  } catch (err: any) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+export const logInWithEmailAndPassword = async (props: EmailLoginProps) => {
+  const { email, password } = props;
+  try {
+    await signInWithEmailAndPassword(getAuth(myApp), email, password);
+  } catch (err: any) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+
+export const registerWithEmailAndPassword = async (props: RegisterEmailProps) => {
+  const { name, email, password, collection_name, default_config } = props;
+  try {
+    const db = getFirestore(myApp);
+    console.log("Creating new user: ", name, email);
+    const res = await createUserWithEmailAndPassword(getAuth(myApp), email, password);
+    const user = res.user;
+    const q = query(collection(db, collection_name), where("uid", "==", user.uid));
+    const docs = await getDocs(q);
+    if (docs.docs.length === 0) {
+      await setDoc(doc(collection(db, collection_name), email), default_config);
+    }
+  } catch (err: any) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+export const sendPasswordReset = async (email: string) => {
+  try {
+    await sendPasswordResetEmail(getAuth(myApp), email);
+    alert("Password reset link sent!");
+  } catch (err: any) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+export const logout = () => {
+  signOut(getAuth(myApp));
 };
